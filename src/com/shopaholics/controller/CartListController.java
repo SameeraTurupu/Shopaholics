c04269b9120510b26dbc434a718ab18472bd7066
DatlaNeelima <neelimadatla288@gmail.com>
2015-07-14 09:48:59 +0530

    Updated

--- src/com/shopaholics/controller/CartListController.java
+++ src/com/shopaholics/controller/CartListController.java
@@ -3,6 +3,7 @@ package com.shopaholics.controller;
 import java.io.IOException;
 import java.sql.SQLException;
 
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,34 +17,49 @@ import com.shopaholics.dao.ProductDAO;
 public class CartListController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doProcess(request, response);
+		try {
+			doProcess(request, response);
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doProcess(request, response);
+		try {
+			doProcess(request, response);
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, NamingException {
 		try{
 			HttpSession hs = request.getSession();
-			String email = (String)hs.getAttribute("email");
+			String usrname = (String)hs.getAttribute("sunm");
 			String id = request.getParameter("id");
 			String product_name = request.getParameter("nm");
 			String stock = request.getParameter("stock");
+			String image = request.getParameter("file");
 			int quantity = Integer.parseInt(stock);
 			int price = Integer.parseInt(request.getParameter("price"));
 			int offer = Integer.parseInt(request.getParameter("offer"));
 			String desc = request.getParameter("desc");
 			ProductDAO ud = new ProductDAO();
-			ProductBean cbean = new ProductBean(id,product_name,desc,quantity, price, offer);
+			ProductBean cbean = new ProductBean(id,product_name,desc,quantity, price, offer,image);
+			if(usrname == null) {
+				System.out.println("hi");
+				response.sendRedirect("login.jsp");
+			}
+			int result = ud.addtocart(cbean,usrname);
 			HttpSession hs2 = request.getSession();
-			hs2.setAttribute("uid",ud.addtocart(cbean,email));
-			//result = ud.addtocart(cbean,usrname);
+			hs.setAttribute("uid", result);
 			HttpSession hs1 = request.getSession();   
 		    int cart = (Integer)hs1.getAttribute("cartno");
 		    int cartno = cart + 1;
 		    hs.setAttribute("cartno", cartno);
-			response.sendRedirect("itemslist.jsp");
+			response.sendRedirect("itemslist.jsp?uid=" + result);
 			}
 			catch (SQLException e) {
 				// TODO: handle exception
--- src/com/shopaholics/controller/CartListDelController.java
+++ src/com/shopaholics/controller/CartListDelController.java
@@ -3,6 +3,7 @@ package com.shopaholics.controller;
 import java.io.IOException;
 import java.sql.SQLException;
 
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,14 +16,24 @@ import com.shopaholics.dao.ProductDAO;
 public class CartListDelController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doProcess(request, response);
+		try {
+			doProcess(request, response);
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doProcess(request, response);
+		try {
+			doProcess(request, response);
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, NamingException {
 		try{
 			HttpSession hs = request.getSession();
 			String usrname = (String)hs.getAttribute("sunm");
